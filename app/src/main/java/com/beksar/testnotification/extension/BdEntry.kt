dd2398d4ca3cf12c5e341f121201b3afc7502bc4
Stap <marsik200894>
2021-08-08 15:26:51 +0600

    12:29

--- app/src/main/java/com/beksar/testnotification/extension/BdEntry.kt
+++ app/src/main/java/com/beksar/testnotification/extension/BdEntry.kt
@@ -6,12 +6,14 @@ import com.beksar.testnotification.adapter.model.ResultRecyclerModel
 import com.beksar.testnotification.service.model.ModelRoom
 import com.beksar.testnotification.ui.main.MainActivity
 import com.example.testroomkotlin.AppDataBase
+import kotlinx.coroutines.*
 
 var roomModel: ArrayList<RecyclerModel> = arrayListOf()
 
 fun writingDatabase(item: ArrayList<ResultRecyclerModel>, db: AppDataBase){
     roomModel.clear()
-    AsyncTask.execute {
+
+    CoroutineScope(Dispatchers.IO).launch {
         db.appDataBase().deleteAllModel()
         for (j in 1..item.size) {
             val dataItem = ModelRoom(j-1, item[j-1].key, item[j-1].country, item[j-1].region)
